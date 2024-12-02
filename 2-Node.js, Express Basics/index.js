38fb117076a87686834c676c8d66fc4219a82d25
Sadakopa Ramakrishnan <nstsrka04@gmail.com>
2024-12-02 20:16:53 +0530

    express basic examples midway

--- 2-Node.js, Express Basics/index.js
+++ 2-Node.js, Express Basics/index.js
@@ -4,10 +4,22 @@ const app = express();
 
 const port = 3000;
 
+function calculateSum(n) {
+    let ans = 0;
+
+    for (let i = 0; i <= n; i++) {
+        ans = ans + i;
+    }
+
+    return ans;
+}
+
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-    res.send("<button>Hello World!</button>");
+    const n = req.query.n;
+    const ans = calculateSum(n);
+    res.send(ans.toString());
 })
 
 app.post('/conversations', (req, res) => {
