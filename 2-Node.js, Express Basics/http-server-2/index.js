89122c30c283d58ea2f94630f6b6746cf9fecdc0
Sadakopa Ramakrishnan <nstsrka04@gmail.com>
2024-12-03 16:07:44 +0530

    express basic

--- 2-Node.js, Express Basics/http-server-2/index.js
+++ 2-Node.js, Express Basics/http-server-2/index.js
@@ -9,7 +9,7 @@ var users = [{
     }]
 }];
 
-
+// query parameters
 app.get('/', function(req, res) {
     const johnKidneys = users[0].kidneys;
     const numberOfKidneys = johnKidneys.length;
@@ -31,17 +31,66 @@ app.get('/', function(req, res) {
 
 })
 
+app.use(express.json());
+// data sent in body
 app.post('/', function(req, res) {
+
+    const isHealthy = req.body.isHealthy;
+
+    users[0].kidneys.push({
+        healthy: isHealthy
+    });
+
+    let done = "done";
+    res.json({
+        done
+    })
 
 })
 
 app.put('/', function(req, res) {
+    for(let i = 0; i < users[0].kidneys.length; i++) {
+        users[0].kidneys[i].healthy = true;
+    }
 
+    res.json({});
 })
 
 app.delete('/', function(req, res) {
+    // you should return a 411
+
+    if (isThereAtleastOneUnhealthyKidney()) {
+        const newKidneys = [];
+        for(let i = 0; i < users[0].kidneys.length; i++) {
+            if (users[0].kidneys[i].healthy) {
+                newKidneys.push({
+                    healthy:true
+                })
+            }
+        }
+    
+        users[0].kidneys = newKidneys;
+        res.json({msg: "done"});
+    
+    } else {
+        res.status(411).json({
+            msg: "You have no bad kidneys"
+        });
+    }
+   
 
 })
+
+
+function isThereAtleastOneUnhealthyKidney() {
+    let atleastOneUnhealthyKidney = false;
+    for(let i = 0; i < users[0].kidneys.length; i++) {
+        if (!users[0].kidneys[i].healthy) {
+            atleastOneUnhealthyKidney = true;
+        }
+    }
+    return atleastOneUnhealthyKidney;
+}
 
 
 app.listen(3000)
